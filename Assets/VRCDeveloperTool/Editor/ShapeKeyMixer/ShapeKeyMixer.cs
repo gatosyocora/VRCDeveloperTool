39a1e09c40d56c673a6cedae237bdd3acf76f645
gatosyocora <kut190316q@gmail.com>
2019-11-04 17:40:54 +0900

    ShapeKeyMixerとSubMeshDeleterを追加

---
+++ Assets/VRCDeveloperTool/Editor/ShapeKeyMixer/ShapeKeyMixer.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+// ver 1.0
+// Copyright (c) 2019 gatosyocora
+
+namespace VRCDeveloperTool
+{
+    public class ShapeKeyMixer : EditorWindow
+    {
+        private SkinnedMeshRenderer renderer;
+
+        private List<string> shapeKeyNames;
+        private bool[] selectedShapeKeys;
+        private bool isOpenedBlendShape = true;
+        private string combinedShapeKeyName = "";
+        private bool deleteOriginShapeKey = true;
+        private Vector2 shapeKeyScrollPos = Vector2.zero;
+
+
+        [MenuItem("VRCDeveloperTool/Mesh/ShapeKey Mixer")]
+        private static void Open()
+        {
+            GetWindow<ShapeKeyMixer>("ShapeKey Mixer");
+        }
+
+        private void OnEnable()
+        {
+            renderer = null;
+            shapeKeyNames = null;
+            selectedShapeKeys = null;
+            isOpenedBlendShape = true;
+            combinedShapeKeyName = "";
+            shapeKeyScrollPos = Vector2.zero;
+        }
+
+        private void OnGUI()
+        {
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                renderer = EditorGUILayout.ObjectField(
+                                "SkinnedMeshRenderer",
+                                renderer,
+                                typeof(SkinnedMeshRenderer),
+                                true
+                            ) as SkinnedMeshRenderer;
+
+
+                if (check.changed)
+                {
+                    if (renderer != null)
+                    {
+                        shapeKeyNames = GetBlendShapeListFromRenderer(renderer);
+                        selectedShapeKeys = new bool[shapeKeyNames.Count()];
+                    }
+                }
+            }
+
+            if (shapeKeyNames != null)
+            {
+                isOpenedBlendShape = EditorGUILayout.Foldout(isOpenedBlendShape, "Shape Keys");
+                if (isOpenedBlendShape)
+                {
+                    using (new EditorGUI.IndentLevelScope())
+                    using (var scroll = new EditorGUILayout.ScrollViewScope(shapeKeyScrollPos, GUI.skin.box))
+                    {
+                        shapeKeyScrollPos = scroll.scrollPosition;
+                        for (int i = 0; i < shapeKeyNames.Count(); i++)
+                        {
+                            selectedShapeKeys[i] = EditorGUILayout.ToggleLeft(shapeKeyNames[i], selectedShapeKeys[i]);
+                        }
+                    }
+                }
+                deleteOriginShapeKey = EditorGUILayout.Toggle("Delete Origin ShapeKey", deleteOriginShapeKey);
+                combinedShapeKeyName = EditorGUILayout.TextField("Mixed ShapeKey Name", combinedShapeKeyName);
+            }
+
+            using (new EditorGUI.DisabledScope(renderer == null || combinedShapeKeyName == "" || (selectedShapeKeys != null && selectedShapeKeys.Sum(x => x ? 1 : 0) <= 1)))
+            {
+                if (GUILayout.Button("Mix ShapeKeys"))
+                {
+                    // 2つ以上が選択されている
+                    if (selectedShapeKeys.Sum(x => x ? 1 : 0) > 1)
+                    {
+                        // 選択されている要素のインデックスの配列
+                        var selectedBlendShapeIndexs = selectedShapeKeys
+                            .Select((isSelect, index) => new { Index = index, Value = isSelect })
+                            .Where(x => x.Value)
+                            .Select(x => x.Index)
+                            .ToArray();
+
+                        MixShapeKey(renderer, selectedBlendShapeIndexs, combinedShapeKeyName, deleteOriginShapeKey);
+                    }
+
+                    shapeKeyNames = GetBlendShapeListFromRenderer(renderer);
+                    selectedShapeKeys = new bool[shapeKeyNames.Count()];
+                }
+            }
+        }
+
+        private bool MixShapeKey(SkinnedMeshRenderer renderer, int[] selectedShapeKeyIndexs, string combinedBlendShapeName, bool deleteOriginShapeKey)
+        {
+            var mesh = renderer.sharedMesh;
+            if (mesh == null) return false;
+
+            var mesh_custom = Instantiate(mesh);
+
+            mesh_custom.ClearBlendShapes();
+
+            int frameIndex = 0;
+            string shapeKeyName;
+            float weight;
+            Vector3[] deltaVertices, deltaNormals, deltaTangents;
+
+            var combinedDeltaVertices = new Vector3[mesh.vertexCount];
+            var combinedDeltaNormals = new Vector3[mesh.vertexCount];
+            var combinedDeltaTangents = new Vector3[mesh.vertexCount];
+            float combinedWeight = 0;
+
+            for (int blendShapeIndex = 0; blendShapeIndex < mesh.blendShapeCount; blendShapeIndex++)
+            {
+                deltaVertices = new Vector3[mesh.vertexCount];
+                deltaNormals = new Vector3[mesh.vertexCount];
+                deltaTangents = new Vector3[mesh.vertexCount];
+                mesh.GetBlendShapeFrameVertices(blendShapeIndex, frameIndex, deltaVertices, deltaNormals, deltaTangents);
+                weight = mesh.GetBlendShapeFrameWeight(blendShapeIndex, frameIndex);
+
+                if (selectedShapeKeyIndexs.Contains(blendShapeIndex))
+                {
+                    for (int i = 0; i < mesh.vertexCount; i++)
+                    {
+                        combinedDeltaVertices[i] += deltaVertices[i];
+                        combinedDeltaNormals[i] += deltaNormals[i];
+                        combinedDeltaTangents[i] += deltaTangents[i];
+                        combinedWeight = Mathf.Max(combinedWeight, weight);
+                    }
+
+                    if (!deleteOriginShapeKey)
+                    {
+                        shapeKeyName = mesh.GetBlendShapeName(blendShapeIndex);
+                        mesh_custom.AddBlendShapeFrame(shapeKeyName, weight, deltaVertices, deltaNormals, deltaTangents);
+                    }
+                }
+                else
+                {
+                    shapeKeyName = mesh.GetBlendShapeName(blendShapeIndex);
+                    mesh_custom.AddBlendShapeFrame(shapeKeyName, weight, deltaVertices, deltaNormals, deltaTangents);
+                }
+            }
+
+            if (selectedShapeKeyIndexs.Length > 0)
+                mesh_custom.AddBlendShapeFrame(combinedShapeKeyName, combinedWeight, combinedDeltaVertices, combinedDeltaNormals, combinedDeltaTangents);
+
+            Undo.RecordObject(renderer, "Renderer " + renderer.name);
+            renderer.sharedMesh = mesh_custom;
+
+            var path = Path.GetDirectoryName(AssetDatabase.GetAssetPath(mesh)) + "/" + mesh.name + "_custom.asset";
+            AssetDatabase.CreateAsset(mesh_custom, AssetDatabase.GenerateUniqueAssetPath(path));
+            AssetDatabase.SaveAssets();
+
+            return true;
+        }
+
+        /// <summary>
+        /// SkinnedMeshRendererのもつメッシュのシェイプキーの名前のリストを取得する
+        /// </summary>
+        /// <param name="renderer"></param>
+        /// <returns></returns>
+        private List<string> GetBlendShapeListFromRenderer(SkinnedMeshRenderer renderer)
+        {
+            List<string> shapeKeyNames = new List<string>();
+            var mesh = renderer.sharedMesh;
+
+            if (mesh != null)
+                for (int i = 0; i < mesh.blendShapeCount; i++)
+                    shapeKeyNames.Add(mesh.GetBlendShapeName(i));
+
+            return shapeKeyNames;
+        }
+    }
+}
---
+++ Assets/VRCDeveloperTool/Editor/SubMeshDeleter/SubMeshDeleter.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+// ver 1.0.1
+// Copyright (c) 2019 gatosyocora
+
+namespace VRCDeveloperTool
+{
+    public class SubMeshDeleter : EditorWindow
+    {
+        private SkinnedMeshRenderer renderer;
+        private List<SubMeshInfo> subMeshList;
+        private int triangleCount = 0;
+
+        private string saveFolder = "Assets/";
+        private bool isOpenedSubMesh = true;
+        private Vector2 subMeshScrollPos = Vector2.zero;
+
+        [MenuItem("VRCDeveloperTool/Mesh/SubMesh Deleter")]
+        private static void Open()
+        {
+            GetWindow<SubMeshDeleter>("SubMeshDeleter");
+        }
+
+        private void OnEnable()
+        {
+            renderer = null;
+            subMeshList = null;
+            triangleCount = 0;
+        }
+
+        private void OnGUI()
+        {
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                renderer = EditorGUILayout.ObjectField(
+                                "SkinnedMeshRenderer",
+                                renderer,
+                                typeof(SkinnedMeshRenderer),
+                                true
+                            ) as SkinnedMeshRenderer;
+
+
+                if (check.changed)
+                {
+                    if (renderer != null)
+                    {
+                        var mesh = renderer.sharedMesh;
+                        if (mesh != null)
+                        {
+                            subMeshList = GetSubMeshList(mesh);
+                            triangleCount = GetMeshTriangleCount(mesh);
+                            saveFolder = GetMeshPath(mesh);
+                        }
+                    }
+                    else
+                    {
+                        subMeshList = null;
+                    }
+                }
+            }
+
+            if (subMeshList != null)
+            {
+                isOpenedSubMesh = EditorGUILayout.Foldout(isOpenedSubMesh, "SubMesh");
+                if (isOpenedSubMesh)
+                {
+                    using (var scroll = new EditorGUILayout.ScrollViewScope(subMeshScrollPos))
+                    {
+                        subMeshScrollPos = scroll.scrollPosition;
+                        for (int i = 0; i < subMeshList.Count(); i++)
+                        {
+                            subMeshList[i].selected = EditorGUILayout.ToggleLeft("subMesh " + (i + 1) + "(" + renderer.sharedMaterials[i].name + "):" + subMeshList[i].triangleCount, subMeshList[i].selected);
+                        }
+                    }
+                        
+                }
+
+            }
+
+            EditorGUILayout.LabelField("Triangle Count", triangleCount+"");
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                EditorGUILayout.LabelField("Mesh SaveFolder", saveFolder);
+
+                if (GUILayout.Button("Select Folder", GUILayout.Width(100)))
+                {
+                    saveFolder = EditorUtility.OpenFolderPanel("Select saved folder", saveFolder, "");
+                    var match = Regex.Match(saveFolder, @"Assets/.*");
+                    saveFolder = match.Value + "/";
+                    if (saveFolder == "/") saveFolder = "Assets/";
+                }
+            }
+
+            using (new EditorGUI.DisabledGroupScope(subMeshList == null || subMeshList.Count() <= 1))
+            {
+                if (GUILayout.Button("Delete SubMesh"))
+                {
+                    DeleteSelectedSubMesh(renderer, subMeshList);
+
+                    var mesh = renderer.sharedMesh;
+                    if (mesh != null)
+                    {
+                        subMeshList = GetSubMeshList(mesh);
+                        triangleCount = GetMeshTriangleCount(mesh);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 選択中のサブメッシュを削除する
+        /// </summary>
+        /// <param name="renderer"></param>
+        /// <param name="subMeshList"></param>
+        /// <returns></returns>
+        private bool DeleteSelectedSubMesh(SkinnedMeshRenderer renderer, List<SubMeshInfo> subMeshList)
+        {
+            // 削除する頂点インデックスのリスト（読み取り専用, 降順）
+            var deleteVerticesIndicesUniqueDescending
+                = subMeshList
+                    .Where(x => x.selected)
+                    .SelectMany(x => x.verticesIndices)
+                    .Distinct()
+                    .OrderByDescending(x => x)
+                    .ToList()
+                    .AsReadOnly();
+
+            // 削除するサブメッシュのインデックスのリスト
+            var deleteSubMeshIndexList
+                = subMeshList
+                    .Select((value, index) => new { Value = value, Index = index })
+                    .Where(x => x.Value.selected)
+                    .Select(x => x.Index)
+                    .ToList()
+                    .AsReadOnly();
+
+            var mesh = renderer.sharedMesh;
+            var mesh_custom = Instantiate(mesh);
+
+            mesh_custom.Clear();
+
+            // 頂点を削除
+            var vertices = mesh.vertices.ToList();
+            var boneWeights = mesh.boneWeights.ToList();
+            var uvs = mesh.uv.ToList();
+            var normals = mesh.normals.ToList();
+            var tangents = mesh.tangents.ToList();
+            var uv2s = mesh.uv2.ToList();
+            var uv3s = mesh.uv3.ToList();
+            var uv4s = mesh.uv4.ToList();
+            foreach (var deleteVertexIndex in deleteVerticesIndicesUniqueDescending)
+            {
+                vertices.RemoveAt(deleteVertexIndex);
+                boneWeights.RemoveAt(deleteVertexIndex);
+                normals.RemoveAt(deleteVertexIndex);
+                tangents.RemoveAt(deleteVertexIndex);
+                if (deleteVertexIndex < uvs.Count())
+                    uvs.RemoveAt(deleteVertexIndex);
+                if (deleteVertexIndex < uv2s.Count())
+                    uv2s.RemoveAt(deleteVertexIndex);
+                if (deleteVertexIndex < uv3s.Count())
+                    uv3s.RemoveAt(deleteVertexIndex);
+                if (deleteVertexIndex < uv4s.Count())
+                    uv4s.RemoveAt(deleteVertexIndex);
+            }
+            mesh_custom.SetVertices(vertices);
+            mesh_custom.boneWeights = boneWeights.ToArray();
+            mesh_custom.normals = normals.ToArray();
+            mesh_custom.tangents = tangents.ToArray();
+            mesh_custom.SetUVs(0, uvs);
+            mesh_custom.SetUVs(1, uv2s);
+            mesh_custom.SetUVs(2, uv3s);
+            mesh_custom.SetUVs(3, uv4s);
+
+            // サブメッシュごとにポリゴンを処理
+            mesh_custom.subMeshCount = mesh.subMeshCount - deleteSubMeshIndexList.Count();
+            var subMeshNumber = 0;
+            for (int subMeshIndex = 0; subMeshIndex < mesh.subMeshCount; subMeshIndex++)
+            {
+                if (deleteSubMeshIndexList.Contains(subMeshIndex)) continue;
+
+                var subMeshTriangles = mesh.GetTriangles(subMeshIndex);
+                // インデックスがずれるので各頂点への対応付けが必要
+                // インデックスが大きいものから順に処理していく
+                // O(n*m)
+                foreach (var deleteVerticesIndex in deleteVerticesIndicesUniqueDescending) // n
+                    for (int i = 0; i < subMeshTriangles.Count(); i++) // m
+                        if (subMeshTriangles[i] > deleteVerticesIndex)
+                            subMeshTriangles[i]--;
+                mesh_custom.SetTriangles(subMeshTriangles, subMeshNumber++);
+            }
+
+            // BlendShapeを設定する
+            string blendShapeName;
+            float frameWeight;
+            var deltaVertices = new Vector3[mesh.vertexCount];
+            var deltaNormals = new Vector3[mesh.vertexCount];
+            var deltaTangents = new Vector3[mesh.vertexCount];
+            List<Vector3> deltaVerticesList, deltaNormalsList, deltaTangentsList;
+            for (int blendshapeIndex = 0; blendshapeIndex < mesh.blendShapeCount; blendshapeIndex++)
+            {
+                blendShapeName = mesh.GetBlendShapeName(blendshapeIndex);
+                frameWeight = mesh.GetBlendShapeFrameWeight(blendshapeIndex, 0);
+                mesh.GetBlendShapeFrameVertices(blendshapeIndex, 0, deltaVertices, deltaNormals, deltaTangents);
+                deltaVerticesList = deltaVertices.ToList();
+                deltaNormalsList = deltaNormals.ToList();
+                deltaTangentsList = deltaTangents.ToList();
+                foreach (var deleteVertexIndex in deleteVerticesIndicesUniqueDescending)
+                {
+                    deltaVerticesList.RemoveAt(deleteVertexIndex);
+                    deltaNormalsList.RemoveAt(deleteVertexIndex);
+                    deltaTangentsList.RemoveAt(deleteVertexIndex);
+                }
+                mesh_custom.AddBlendShapeFrame(blendShapeName, frameWeight,
+                    deltaVerticesList.ToArray(),
+                    deltaNormalsList.ToArray(),
+                    deltaTangentsList.ToArray());
+            }
+
+            AssetDatabase.CreateAsset(mesh_custom, AssetDatabase.GenerateUniqueAssetPath(saveFolder + mesh.name + "_deleteSubmesh.asset"));
+            AssetDatabase.SaveAssets();
+
+            Undo.RecordObject(renderer, "Change mesh " + mesh_custom.name);
+            renderer.sharedMesh = mesh_custom;
+
+            // 削除したサブメッシュのマテリアルを参照から外す
+            var materials = renderer.sharedMaterials.ToList();
+            for (var index = materials.Count() - 1; index >= 0; index--)
+                if (deleteSubMeshIndexList.Contains(index))
+                    materials.RemoveAt(index);
+            renderer.sharedMaterials = materials.ToArray();
+
+            return true;
+        }
+
+        /// <summary>
+        /// サブメッシュのリストを取得する
+        /// </summary>
+        /// <param name="mesh"></param>
+        /// <returns></returns>
+        private List<SubMeshInfo> GetSubMeshList(Mesh mesh)
+        {
+            List<SubMeshInfo> subMeshList = new List<SubMeshInfo>();
+
+            for (int subMeshIndex = 0; subMeshIndex < mesh.subMeshCount; subMeshIndex++)
+            {
+                var meshInfo = new SubMeshInfo(mesh, subMeshIndex);
+                subMeshList.Add(meshInfo);
+            }
+
+            return subMeshList;
+        }
+
+        /// <summary>
+        /// Meshのポリゴン数を取得する
+        /// </summary>
+        /// <param name="mesh"></param>
+        /// <returns></returns>
+        private int GetMeshTriangleCount(Mesh mesh)
+        {
+            return mesh.triangles.Length / 3;
+        }
+
+        /// <summary>
+        /// mesh保存先のパスを取得する
+        /// </summary>
+        /// <param name="Mesh"></param>
+        /// <returns></returns>
+        private string GetMeshPath(Mesh mesh)
+        {
+            return Path.GetDirectoryName(AssetDatabase.GetAssetPath(mesh))+"/";
+        }
+
+        public class SubMeshInfo
+        {
+            public int subMeshIndex;
+            public int[] verticesIndices;
+            public int vertexCount;
+            public int triangleCount;
+            public bool selected = false;
+
+            public SubMeshInfo(Mesh mesh, int subMeshIndex)
+            {
+                this.subMeshIndex = subMeshIndex;
+                this.verticesIndices = mesh.GetIndices(subMeshIndex);
+                vertexCount = verticesIndices.Length;
+                triangleCount = mesh.GetTriangles(subMeshIndex).Length / 3;
+            }
+        }
+    }
+}
